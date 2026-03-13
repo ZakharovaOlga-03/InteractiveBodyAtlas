cac44e32ad477d1e4a39dc5d279d3fca9588ca75
Olga Zakharova <amaranta.black.03@gmail.com>
2026-03-13 23:58:43 +0300

    экспорт

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 // main.js — точка входа для CDN
 import { CSVGroupsManager } from './csv-groups.js';
 import { AtlasScene } from './AtlasScene.js';
+import {MarkerManager} from './MarkerManager.js'
+import {MarkerUI} from './MarkerUI.js'
 
 // Реэкспортируем всё
-export { CSVGroupsManager, AtlasScene };
+export { CSVGroupsManager, AtlasScene, MarkerManager, MarkerUI };
 export * from './csv-groups.js';
 
 // Для обратной совместимости
-export default { CSVGroupsManager, AtlasScene };
+export default { CSVGroupsManager, AtlasScene, MarkerManager, MarkerUI };
